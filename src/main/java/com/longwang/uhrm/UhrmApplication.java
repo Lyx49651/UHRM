8b84e5c86f7a5f5776fd20c8ded14eb56f5b19f8
Lyx49651 <52563902+Lyx49651@users.noreply.github.com>
2020-08-26 10:16:03 +0800

    Initial commit

---
+++ src/main/java/com/longwang/uhrm/UhrmApplication.java
@@ -0,0 +1,13 @@
+package com.longwang.uhrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UhrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UhrmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/longwang/uhrm/UhrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.longwang.uhrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UhrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
