051d411718823b9399addbcb9c5a4f688cbe083a
Kanglun Zhao <wobuhuicode@outlook.com>
2020-08-26 11:06:03 +0800

    appliication

---
+++ src/main/java/com/longwang/uhrm/Test.java
@@ -0,0 +1,12 @@
+package com.longwang.uhrm;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+
+    }
+}
