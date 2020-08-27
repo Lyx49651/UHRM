3725fd2373cf4900a6c76afbc33fac46ecc4f5f7
Corleone352 <61043674+Corleone352@users.noreply.github.com>
2020-08-27 20:40:21 +0800

    员工录入页面

--- src/main/java/com/longwang/uhrm/ServletInitializer.java
+++ src/main/java/com/longwang/uhrm/ServletInitializer.java
@@ -4,7 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@MapperScan("com.longwang.uhrm.Entity.mapper")
 public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
--- src/main/java/com/longwang/uhrm/UhrmApplication.java
+++ src/main/java/com/longwang/uhrm/UhrmApplication.java
@@ -3,7 +3,6 @@ package com.longwang.uhrm;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @MapperScan("com.longwang.uhrm.Entity.mapper")
 @SpringBootApplication//排除自动配置
