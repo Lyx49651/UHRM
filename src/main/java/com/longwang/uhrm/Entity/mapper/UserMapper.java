dabc7c0630e24ab1bb093079c67927984d0e6dca
rickyEI <744546785@qq.com>
2020-08-27 20:49:48 +0800

    user

--- src/main/java/com/longwang/uhrm/Entity/mapper/UserMapper.java
+++ src/main/java/com/longwang/uhrm/Entity/mapper/UserMapper.java
@@ -1,16 +1,29 @@
 package com.longwang.uhrm.Entity.mapper;
 
 import com.longwang.uhrm.Entity.User;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
+    @Select("select *from User where id = #{idUser}")
+    public User getUserById(int idUser);
+
+    @Select("select *from User where name = #{name}")
+    public User getUserByName(String name);
+
     @Select("select password from User where telephone = #{telephone}")
     public String getPassword(String telephone);
 
     @Insert("insert into User(name, sex, IDCard, photo, address, age, mailAddress, telephone, Post_idPost, password) " +
             "values(#{name}, #{sex}, #{IDCard}, #{photo}, #{address}, #{age}, #{mailAddress}, #{telephone}, #{postIdPost}, #{password})")
     public int insertUser(User user);
+
+    @Delete("delete from user where idUser = #{idUser}")
+    public boolean deleteUserById(int idUser);
+
+    @Delete("delete from user where name = #{name}")
+    public boolean deleteUserByName(String name);
 }
