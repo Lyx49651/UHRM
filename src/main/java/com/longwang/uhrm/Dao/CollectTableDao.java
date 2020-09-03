152f825fab1913628ddd179f4065f18b3ce6dc2d
rickyEI <744546785@qq.com>
2020-09-03 17:06:07 +0800

    CollectTableDao数据库操作

--- src/main/java/com/longwang/uhrm/Dao/CollectTableDao.java
+++ src/main/java/com/longwang/uhrm/Dao/CollectTableDao.java
@@ -19,4 +19,15 @@ public class CollectTableDao {
     public boolean changeStatusById(String status, int id){ return collectTableMapper.changeStatusById(status, id) == 1; }
     public boolean insertCollectTable(CollectTable collectTable) {return collectTableMapper.insertCollectTable(collectTable) == 1;}
 
+    //删除
+    public boolean deleteById(int id) {
+        return collectTableMapper.deleteByID(id);
+    }
+
+    //更新collect表为passed
+    public boolean updatePassed(int id, String recutimentNumber){
+        return  collectTableMapper.updateStatus(id,recutimentNumber);
+    }
+
+
 }
