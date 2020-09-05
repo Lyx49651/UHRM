b31958e11e5a41d784c16cdfb19c553b9bea1eac
Lyx49651 <52563902+Lyx49651@users.noreply.github.com>
2020-09-05 10:45:48 +0800

    。。

--- src/main/java/com/longwang/uhrm/Controller/ViewController.java
+++ src/main/java/com/longwang/uhrm/Controller/ViewController.java
@@ -659,8 +659,18 @@ public class ViewController {
 
     //跳转到合同查询
     @RequestMapping(method = RequestMethod.GET,value = "/to_contract_query")
-    public String to_contract_query(Model model){
+    public String to_contract_query(HttpServletRequest request,Model model){
         List<Contract> list = employeeArchivesDao.findAllContract();
+        HttpSession httpSession = request.getSession();
+        String type = (String) httpSession.getAttribute("type");
+        String name = (String) httpSession.getAttribute("name");
+        if(name != null && type.equals("employee")){ 
+            model.addAttribute("typeBoolean",true);
+            model.addAttribute("name",httpSession.getAttribute("name"));
+        }else if(name != null && type.equals("user")){
+            model.addAttribute("typeBoolean",false);
+            model.addAttribute("name",httpSession.getAttribute("name"));
+        }
         model.addAttribute("list",list);
         return "contract_search";
     }
@@ -730,7 +740,17 @@ public class ViewController {
 
     //to新增合同
     @RequestMapping(method = RequestMethod.GET,value = "/to_add_contract")
-    public String to_add_contract(){
+    public String to_add_contract(HttpServletRequest httpServletRequest,Model model){
+        HttpSession httpSession = httpServletRequest.getSession();
+        String type = (String) httpSession.getAttribute("type");
+        String name = (String) httpSession.getAttribute("name");
+        if(name != null && type.equals("employee")){
+            model.addAttribute("typeBoolean",true);
+            model.addAttribute("name",httpSession.getAttribute("name"));
+        }else if(name != null && type.equals("user")){
+            model.addAttribute("typeBoolean",false);
+            model.addAttribute("name",httpSession.getAttribute("name"));
+        }
         return "contract_import";
     }
 
